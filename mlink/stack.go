3ae07bf8881567fe45c1d4af675a3ffdd05c2fb9
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-12-25 12:07:57 -0800

    mlink: implement Stack[T] using a slice
    Instead of allocating pointer structures for a stack, use the append trick.

--- mlink/stack.go
+++ mlink/stack.go
@@ -1,40 +1,50 @@
 package mlink
 
-// A Stack is a linked last-in, first-out sequence of values.
+// A Stack is a last-in, first-out sequence of values.
 // A zero value is ready for use.
 type Stack[T any] struct {
-	list List[T]
-	size int
+	list []T
 }
 
 // NewStack constructs a new empty stack.
 func NewStack[T any]() *Stack[T] { return new(Stack[T]) }
 
 // Push adds an entry for v to the top of s.
-func (s *Stack[T]) Push(v T) { s.list.At(0).Push(v); s.size++ }
+func (s *Stack[T]) Push(v T) { s.list = append(s.list, v) }
 
 // IsEmpty reports whether s is empty.
-func (s *Stack[T]) IsEmpty() bool { return s.list.IsEmpty() }
+func (s *Stack[T]) IsEmpty() bool { return len(s.list) == 0 }
 
 // Clear discards all the values in s, leaving it empty.
-func (s *Stack[T]) Clear() { s.list.Clear(); s.size = 0 }
+func (s *Stack[T]) Clear() { s.list = s.list[:0] }
 
 // Top reports whether s is non-empty, and if so returns its top value.
-func (s *Stack[T]) Top() (T, bool) { return s.list.Peek(0) }
+func (s *Stack[T]) Top() (T, bool) {
+	if len(s.list) == 0 {
+		var zero T
+		return zero, false
+	}
+	return s.list[len(s.list)-1], true
+}
 
 // Peek reports whether s has value at offset n from the top of the stack, and
 // if so returns its value. Peek(0) is equivalent to Top.
 //
 // Peek will panic if n < 0.
-func (s *Stack[T]) Peek(n int) (T, bool) { return s.list.Peek(n) }
+func (s *Stack[T]) Peek(n int) (T, bool) {
+	if n > len(s.list) {
+		var zero T
+		return zero, false
+	}
+	return s.list[len(s.list)-1-n], true
+}
 
 // Pop reports whether s is non-empty, and if so it removes and returns its top
 // value.
 func (s *Stack[T]) Pop() (T, bool) {
-	out, ok := s.list.Peek(0)
+	out, ok := s.Top()
 	if ok {
-		s.list.At(0).Remove()
-		s.size--
+		s.list = s.list[:len(s.list)-1]
 	}
 	return out, ok
 }
@@ -42,7 +52,14 @@ func (s *Stack[T]) Pop() (T, bool) {
 // Each calls f with each value in s, in order from newest to oldest.
 // If f returns false, Each stops and returns false.
 // Otherwise, Each returns true after visiting all elements of s.
-func (s *Stack[T]) Each(f func(T) bool) bool { return s.list.Each(f) }
+func (s *Stack[T]) Each(f func(T) bool) bool {
+	for i := len(s.list) - 1; i >= 0; i-- {
+		if !f(s.list[i]) {
+			return false
+		}
+	}
+	return true
+}
 
 // Len reports the number of elements in s. This is a constant-time operation.
-func (s *Stack[T]) Len() int { return s.size }
+func (s *Stack[T]) Len() int { return len(s.list) }
