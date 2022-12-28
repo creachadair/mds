fd0d35808a6ff0c854af12aa78e046d6f0b609b4
M. J. Fromberger <fromberger@tailscale.com>
2022-12-27 18:06:55 -0800

    mlink: various improvements to the Ring[T] type

--- mlink/ring.go
+++ mlink/ring.go
@@ -1,54 +1,91 @@
 package mlink
 
-// A Ring is a doubly-linked circular chain of one or more data items.
-// There is no designated beginning of a ring; each element in the chain is a
-// valid entry point for the entire ring.
+// A Ring is a doubly-linked circular chain of data items.  There is no
+// designated beginning or end of a ring; each element is a valid entry point
+// for the entire ring. A ring with no elements is represented as nil.
 type Ring[T any] struct {
 	Value T
 
 	prev, next *Ring[T]
 }
 
-// NewRing constructs a new ring containing only the item given.
-func NewRing[T any](item T) *Ring[T] {
-	r := &Ring[T]{Value: item}
-	r.prev = r
-	r.next = r
+// NewRing constructs a new ring with n zero-valued elements.
+// If n <= 0, NewRing returns nil.
+func NewRing[T any](n int) *Ring[T] {
+	if n <= 0 {
+		return nil
+	}
+	r := newRing[T]()
+	for n > 1 {
+		elt := newRing[T]()
+		elt.next = r.next
+		r.next.prev = elt
+		elt.prev = r
+		r.next = elt
+		n--
+	}
 	return r
 }
 
-// Adjoin splices ring s into r.  If r = [r1 ... rn] and s = [s1 ... sm], the
-// resulting ring is
+// RingOf constructs a new ring containing the given elements.
+func RingOf[T any](vs ...T) *Ring[T] {
+	r := NewRing[T](len(vs))
+	cur := r
+	for _, v := range vs {
+		cur.Value = v
+		cur = cur.Next()
+	}
+	return r
+}
+
+// Len reports the number of elements in r. If r == nil, Len is 0.
+// This operation takes time proportional to the size of the ring.
+func (r *Ring[T]) Len() int {
+	if r == nil {
+		return 0
+	}
+	var n int
+	scan(r, func(*Ring[T]) bool { n++; return true })
+	return n
+}
+
+// IsEmpty reports whether r is the empty ring.
+func (r *Ring[T]) IsEmpty() bool { return r == nil }
+
+// Join splices ring s into a non-empty ring r. There are two cases:
 //
-//	rs = [r1 s1 ... sm r2 ... rn]
+// If r and s belong to different rings, [r1 ... rn] and [s1 ... sm], the
+// elements of s are spliced in after r and the resulting ring is:
 //
-// where r remains at r1 and s remains at s1.
-func (r *Ring[T]) Adjoin(s *Ring[T]) {
+//	[r1 s1 ... sm r2 ... rn]
+//
+// In this case Join returns the ring [r2 ... ].
+//
+// If r and s belong to the same ring, [r1 r2 ... ri s1 ... sm ... rn], then
+// the loop of the ring from r2 ... si is spliced out of r and the resulting
+// ring is:
+//
+//	[r1 s1 ... sm ... rn]
+//
+// In this case Join returns the ring [r2 ... ri] that was spliced out.  This
+// may be empty (nil) if there were no elements between r1 and s1.
+func (r *Ring[T]) Join(s *Ring[T]) *Ring[T] {
+	if r == s || r.next == s {
+		return nil // same ring, nothing to do
+	}
 	rnext, sprev := r.next, s.prev
 
 	r.next = s         // successor of r is now s
 	s.prev = r         // predecessor of s is now r
 	sprev.next = rnext // successor of s end is now rnext
 	rnext.prev = sprev // predecessor of rnext is now s end
-}
-
-// Add splices vs into r. If r = [r1 ... 4n] and vs = [v1 ... vm], the
-// resulting ring is
-//
-//	r = [r1 v1 ... vm r2 ... rn]
-//
-// where r rmains at r1.
-func (r *Ring[T]) Add(vs ...T) *Ring[T] {
-	for i := len(vs) - 1; i >= 0; i-- {
-		r.Adjoin(NewRing(vs[i]))
-	}
-	return r
+	return rnext
 }
 
 // Pop detaches r from its ring, leaving it linked only to itself.
 // It returns r to permit method chaining.
 func (r *Ring[T]) Pop() *Ring[T] {
-	if r.prev != r {
+	if r != nil && r.prev != r {
 		rprev, rnext := r.prev, r.next
 		rprev.next = r.next
 		rnext.prev = r.prev
@@ -59,17 +96,27 @@ func (r *Ring[T]) Pop() *Ring[T] {
 }
 
 // Next returns the successor of r (which may be r itself).
+// This will panic if r == nil.
 func (r *Ring[T]) Next() *Ring[T] { return r.next }
 
 // Prev returns the predecessor of r (which may be r itself).
+// This will panic if r == nil.
 func (r *Ring[T]) Prev() *Ring[T] { return r.prev }
 
 // Each calls f with each value in r, in circular order. If f returns false,
 // Each stops and returns false.  Otherwise, Each returns true after visiting
 // all elements of r.
 func (r *Ring[T]) Each(f func(v T) bool) bool {
+	return scan(r, func(cur *Ring[T]) bool { return f(cur.Value) })
+}
+
+func scan[T any](r *Ring[T], f func(*Ring[T]) bool) bool {
+	if r == nil {
+		return true
+	}
+
 	cur := r
-	for f(cur.Value) {
+	for f(cur) {
 		if cur.next == r {
 			return true
 		}
@@ -77,3 +124,5 @@ func (r *Ring[T]) Each(f func(v T) bool) bool {
 	}
 	return false
 }
+
+func newRing[T any]() *Ring[T] { r := new(Ring[T]); r.next = r; r.prev = r; return r }
--- mlink/ring_test.go
+++ mlink/ring_test.go
@@ -8,9 +8,10 @@ import (
 )
 
 func TestRing(t *testing.T) {
-	r := mlink.NewRing[int](0)
+	var r *mlink.Ring[int]
 
 	rcheck := func(r *mlink.Ring[int], want ...int) {
+		t.Helper()
 		var got []int
 		r.Each(func(v int) bool {
 			got = append(got, v)
@@ -19,30 +20,70 @@ func TestRing(t *testing.T) {
 		if diff := cmp.Diff(want, got); diff != "" {
 			t.Errorf("Wrong contents (-got, +want):\n%s", diff)
 		}
+		if got := r.Len(); got != len(want) {
+			t.Errorf("Wrong length: got %v, want %v", got, len(want))
+		}
 	}
-	check := func(want ...int) { rcheck(r, want...) }
+	check := func(want ...int) { t.Helper(); rcheck(r, want...) }
 
+	// Initial conditions.
+	check()
+	rcheck(mlink.NewRing[int](0))
+	rcheck(mlink.RingOf[int]())
+	rcheck(mlink.NewRing[int](4), 0, 0, 0, 0)
+
+	r = mlink.RingOf(0)
 	check(0)
+
+	// Popping a single-element chain should be a noop.
 	r.Pop()
 	check(0)
-	r.Add(1, 2, 3)
+
+	r.Join(mlink.RingOf(1, 2, 3))
 	check(0, 1, 2, 3)
+
+	// Joining r to itself should do nothing.
+	r.Join(r)
+	check(0, 1, 2, 3)
+
+	// Test removing items from the ring.
 	r.Next().Pop()
 	check(0, 2, 3)
 	r.Prev().Pop()
 	check(0, 2)
-	r.Next().Add(4, 5, 6)
+
+	// Test adding items to various places in the ring.
+	r.Next().Join(mlink.RingOf(4, 5, 6))
 	check(0, 2, 4, 5, 6)
-	r.Prev().Add(7)
+	r.Prev().Join(mlink.RingOf(7))
 	check(0, 2, 4, 5, 6, 7)
-	r = r.Next()
+
+	// Check circular equivalence.
+	r = r.Next().Next()
+	check(4, 5, 6, 7, 0, 2)
+	r = r.Prev()
 	check(2, 4, 5, 6, 7, 0)
 	r.Prev().Pop()
 	check(2, 4, 5, 6, 7)
 
-	s := mlink.NewRing(10).Add(20, 30)
+	s := mlink.RingOf(10, 20, 30)
 	rcheck(s, 10, 20, 30)
-	r.Next().Adjoin(s)
-	check(2, 4, 10, 20, 30, 5, 6, 7)
-	rcheck(s, 10, 20, 30, 5, 6, 7, 2, 4)
+
+	// Joining two separate rings splices in.
+	{
+		x := r.Next().Join(s)
+		check(2, 4, 10, 20, 30, 5, 6, 7)
+		//    ^- r  ^- s        ^- x
+		rcheck(s, 10, 20, 30, 5, 6, 7, 2, 4)
+		//        ^- s        ^- x     ^- r
+		rcheck(x, 5, 6, 7, 2, 4, 10, 20, 30)
+		//        ^- x     ^- r  ^- s
+	}
+
+	// Joining members of the same ring splices out.
+	rcheck(r.Join(s), 4) // just the excised part
+	check(2, 10, 20, 30, 5, 6, 7)
+
+	rcheck(r.Join(r.Next())) // nothing was removed
+	check(2, 10, 20, 30, 5, 6, 7)
 }
