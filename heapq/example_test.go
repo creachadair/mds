4f8e7d0034f9f115ed464a572834486e6bf92414
M. J. Fromberger <fromberger@tailscale.com>
2022-12-29 16:35:53 -0800

    heapq: fix example name

--- heapq/example_test.go
+++ heapq/example_test.go
@@ -8,19 +8,25 @@ import (
 	"github.com/creachadair/mds/heapq"
 )
 
-func Example_InPlaceHeapSort() {
+func Example_inPlaceHeapSort() {
+	// Fill a buffer with some random data for example purposes.
 	buf := make([]int, 25)
 	for i := range buf {
 		buf[i] = rand.Intn(100) - 25
 	}
 
+	// Put the data into a heap. Note that the comparison function here puts
+	// greater elements at the top.
 	q := heapq.NewWithData(func(a, b int) bool {
 		return a > b
 	}, buf)
 
+	// Pull the items off the heap. Because the heap is using the buffer we
+	// already allocated, each item removed leaves an empty slot at the end of
+	// the array.
 	for i := len(buf) - 1; !q.IsEmpty(); i-- {
 		v, _ := q.Pop()
-		buf[i] = v
+		buf[i] = v // N.B. Order matters here: Pop before update!
 	}
 	fmt.Println(sort.IntsAreSorted(buf))
 	// Output:
