398ac09215dc8c30abe90bdf5afd2731f2b067f3
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-01-12 14:52:37 -0800

    slice: add Dedup function

--- slice/slice.go
+++ slice/slice.go
@@ -63,3 +63,58 @@ func Partition[V any](vs []V, keep func(V) bool) int {
 	}
 	return i
 }
+
+// Dedup rearranges the elements of vs in-place to deduplicate consecutive runs
+// of identical elements.  It returns a prefix of vs that contains the first
+// element of each run found, in their original relative order.  It takes time
+// proportional to len(vs) and does not allocate storage outside the slice.
+//
+// The returned slice will contain (non-consecutive) duplicates if and only if
+// vs is not in sorted order at input. If vs is sorted at input (in either
+// direction), the elements of the prefix are exactly the unique first elements
+// of the input.
+func Dedup[T comparable](vs []T) []T {
+	if len(vs) == 0 {
+		return vs
+	}
+
+	// Setup:
+	//   i  : the location of the first element of the next run
+	//   j  : runs forward from i looking for the end of the run
+	//
+	i, j := 0, 1
+	for {
+		// Scan forward from i for an element different from vs[i].
+		for j < len(vs) && vs[i] == vs[j] {
+			j++
+		}
+
+		// If there are no further distinct elements, we're done.  The item at
+		// position i is the beginning of the last run in the slice.
+		if j == len(vs) {
+			return vs[:i+1]
+		}
+
+		// Reaching here, the slice looks like this:
+		//
+		//   [a b c d d d d d d d e ? ? ?]
+		//    0     i             j       n
+		//
+		// where a, b, c, d are distinct from their neighbors.
+
+		// Otherwise, we have found the first item of a new run at j.
+		// Move i forward to the next slot and (if necessary) swap vs[j] into it.
+		i++
+		if j > i {
+			// A swap is unnecessary (though harmless) if j is already the next slot.
+			vs[i], vs[j] = vs[j], vs[i]
+		}
+		j++
+
+		// Now:
+		//               swapped
+		//            v-----------v
+		//   [a b c d e d d d d d d ? ? ?]
+		//    0       i             j     n
+	}
+}
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-type testCase[T any] struct {
+type testCase[T comparable] struct {
 	desc    string
 	input   []T
 	want    []T // desired output, including order
@@ -42,7 +42,7 @@ func TestPartition(t *testing.T) {
 			[]string{"share", "software"}, 2,
 			func(s string) bool { return len(s) > 4 }},
 	} {
-		t.Run(test.desc, test.run)
+		t.Run(test.desc, test.partition)
 	}
 	for _, test := range []testCase[int]{
 		{"Less than 5",
@@ -52,11 +52,31 @@ func TestPartition(t *testing.T) {
 			[]int{2, 2, 4, 1, 1, 3, 6, 6, 6, 5, 8}, []int{2, 2, 4, 6, 6, 6, 8}, 7,
 			func(z int) bool { return z%2 == 0 }},
 	} {
-		t.Run(test.desc, test.run)
+		t.Run(test.desc, test.partition)
 	}
 }
 
-func (tc *testCase[T]) run(t *testing.T) {
+func TestDedup(t *testing.T) {
+	for _, test := range []testCase[int]{
+		{"Nil", nil, nil, 0, nil},
+		{"Nil", []int{}, nil, 0, nil},
+		{"NoRuns", []int{1, 3, 2, 4}, []int{1, 3, 2, 4}, 4, nil},
+		{"Single", []int{5, 5, 5, 5, 5}, []int{5}, 1, nil},
+		{"Two", []int{2, 2, 2, 3, 3, 3}, []int{2, 3}, 2, nil},
+		{"Repeat", []int{1, 3, 3, 1}, []int{1, 3, 1}, 3, nil},
+		{"NoRunsAsc", []int{0, 1, 2, 3, 4}, []int{0, 1, 2, 3, 4}, 5, nil},
+		{"NoRunsDesc", []int{10, 9, 8, 7}, []int{10, 9, 8, 7}, 4, nil},
+		{"RunsAsc", []int{0, 1, 1, 1, 2, 2, 3}, []int{0, 1, 2, 3}, 4, nil},
+		{"RunsDesc", []int{5, 5, 5, 3, 3, 1, 1, 0}, []int{5, 3, 1, 0}, 4, nil},
+
+		// Runs:           a  b---  c  b---  d---  e---------
+		{"Unsorted", []int{1, 0, 0, 9, 0, 0, 3, 3, 2, 2, 2, 2}, []int{1, 0, 9, 0, 3, 2}, 6, nil},
+	} {
+		t.Run(test.desc, test.dedup)
+	}
+}
+
+func (tc *testCase[T]) partition(t *testing.T) {
 	t.Helper()
 
 	cp := copyOf(tc.input)
@@ -70,9 +90,25 @@ func (tc *testCase[T]) run(t *testing.T) {
 	t.Logf("After partition: %+v ~ %+v", cp[:gotPos], cp[gotPos:])
 	diff := cmp.Diff(tc.want, cp[:gotPos], cmpopts.EquateEmpty())
 	if diff != "" {
-		if diff != "" {
-			t.Errorf("Partition result (-want, +got)\n%s", diff)
-		}
+		t.Errorf("Partition result (-want, +got)\n%s", diff)
+	}
+}
+
+func (tc *testCase[T]) dedup(t *testing.T) {
+	t.Helper()
+
+	cp := copyOf(tc.input)
+	t.Logf("Input: %+v", cp)
+
+	got := slice.Dedup(cp)
+	if len(got) != tc.wantPos {
+		t.Errorf("Dedup index: got %d, want %d", len(got), tc.wantPos)
+	}
+
+	t.Logf("After dedup: %+v ~ %+v", got, cp[len(got):])
+	diff := cmp.Diff(tc.want, got, cmpopts.EquateEmpty())
+	if diff != "" {
+		t.Errorf("Dedup result (-want, +got)\n%s", diff)
 	}
 }
 
