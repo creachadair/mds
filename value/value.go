1bf4c22f52998d47859a236c48b9ff93a17ed62a
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-08-18 07:46:25 -0700

    value: add a package to adapt value types

---
+++ value/value.go
@@ -0,0 +1,16 @@
+// Package value defines comparisons and adapters for value types.
+package value
+
+// LessCompare converts a less function, which reports whether its first
+// argument precedes its second in an ordering relation, into a comparison that
+// returns -1 if a < b, 0 if a == b, and 1 if a > b in the same relation.
+func LessCompare[T any](less func(a, b T) bool) func(a, b T) int {
+	return func(a, b T) int {
+		if less(a, b) {
+			return -1
+		} else if less(b, a) {
+			return 1
+		}
+		return 0
+	}
+}
---
+++ value/value_test.go
@@ -0,0 +1,42 @@
+package value_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/creachadair/mds/value"
+)
+
+func TestLessCompare(t *testing.T) {
+	for _, less := range [](func(a, b int) bool){
+		func(a, b int) bool { return a < b },
+		func(a, b int) bool { return a > b },
+	} {
+		cmp := value.LessCompare(less)
+
+		for i := 0; i < 1000; i++ {
+			m := rand.Intn(1000) - 500
+			n := rand.Intn(1000) - 500
+
+			mn, nm := less(m, n), less(n, m)
+			if mn && nm {
+				t.Fatalf("Invalid less function: %d < %d and %d < %d", m, n, n, m)
+			}
+			diff := cmp(m, n)
+			switch {
+			case mn:
+				if diff >= 0 {
+					t.Errorf("Compare %d %d: got %v, want ≥ 0", m, n, diff)
+				}
+			case nm:
+				if diff <= 0 {
+					t.Errorf("Compare %d %d: got %v, want ≤ 0", m, n, diff)
+				}
+			default:
+				if diff != 0 {
+					t.Errorf("Compare %d %d: got %v, want 0", m, n, diff)
+				}
+			}
+		}
+	}
+}
