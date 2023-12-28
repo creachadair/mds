270dce207742fe20b88aa7bdbeddf08a056c22ea
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-12-28 12:03:42 -0800

    compare: add a package for comparison functions
    Move LessCompare from package value.

---
+++ compare/compare.go
@@ -0,0 +1,16 @@
+// Package compare contains support functions for comparison of values.
+package compare
+
+// LessCompare converts a less function, which reports whether its first
+// argument precedes its second in an ordering relation, into a comparison that
+// returns -1 if a < b, 0 if a == b, and 1 if a > b in the same relation.
+func LessCompare[T any](less func(a, b T) bool) func(a, b T) int {
+	return func(a, b T) int {
+		if less(a, b) {
+			return -1
+		} else if less(b, a) {
+			return 1
+		}
+		return 0
+	}
+}
--- value/value.go
+++ value/value.go
@@ -1,19 +1,5 @@
-// Package value defines comparisons and adapters for value types.
+// Package value defines adapters for value types.
 package value
-
-// LessCompare converts a less function, which reports whether its first
-// argument precedes its second in an ordering relation, into a comparison that
-// returns -1 if a < b, 0 if a == b, and 1 if a > b in the same relation.
-func LessCompare[T any](less func(a, b T) bool) func(a, b T) int {
-	return func(a, b T) int {
-		if less(a, b) {
-			return -1
-		} else if less(b, a) {
-			return 1
-		}
-		return 0
-	}
-}
 
 // Ptr returns a pointer to its argument type containing v.
 func Ptr[T any](v T) *T { return &v }
