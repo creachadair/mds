1d83d909339c1df5f0f9ca04b276fa7b64c9327b
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-01-05 08:23:41 -0800

    value: add At and AtDefault

--- value/value.go
+++ value/value.go
@@ -3,3 +3,20 @@ package value
 
 // Ptr returns a pointer to its argument type containing v.
 func Ptr[T any](v T) *T { return &v }
+
+// At returns the value pointed to by p, or zero if p == nil.
+func At[T any](p *T) T {
+	if p == nil {
+		var zero T
+		return zero
+	}
+	return *p
+}
+
+// AtDefault returns the value pointed to by p, or dflt if p == nil.
+func AtDefault[T any](p *T, dflt T) T {
+	if p == nil {
+		return dflt
+	}
+	return *p
+}
--- value/value_test.go
+++ value/value_test.go
@@ -16,3 +16,35 @@ func TestPtr(t *testing.T) {
 		t.Errorf("Got p1=%q, p2=%q; wanted both foo", *p1, *p2)
 	}
 }
+
+func TestAt(t *testing.T) {
+	tests := []struct {
+		input *string
+		want  string
+	}{
+		{nil, ""},
+		{value.Ptr("foo"), "foo"},
+	}
+	for _, tc := range tests {
+		if got := value.At(tc.input); got != tc.want {
+			t.Errorf("At(%p): got %q, want %q", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestAtDefault(t *testing.T) {
+	tests := []struct {
+		input *string
+		dflt  string
+		want  string
+	}{
+		{nil, "", ""},
+		{nil, "foo", "foo"},
+		{value.Ptr("foo"), "bar", "foo"},
+	}
+	for _, tc := range tests {
+		if got := value.AtDefault(tc.input, tc.dflt); got != tc.want {
+			t.Errorf("AtDefault(%p, %q): got %q, want %q", tc.input, tc.dflt, got, tc.want)
+		}
+	}
+}
