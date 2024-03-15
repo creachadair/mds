f21238d6c885ba5ba40e64cdc209ddb471e8f8e1
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-03-14 18:49:48 -0700

    mdiff: add a package to compute string diffs

---
+++ mdiff/example_test.go
@@ -0,0 +1,50 @@
+package mdiff_test
+
+import (
+	"os"
+
+	"github.com/creachadair/mds/mdiff"
+)
+
+func ExampleFormat() {
+	diff := mdiff.New(
+		[]string{"I", "saw", "three", "mice", "running", "away"},
+		[]string{"three", "blind", "mice", "ran", "home"},
+	)
+	mdiff.Format(os.Stdout, diff, nil)
+	// Output:
+	//
+	// 1,2d0
+	// < I
+	// < saw
+	// 3a2
+	// > blind
+	// 5,6c4,5
+	// < running
+	// < away
+	// ---
+	// > ran
+	// > home
+}
+
+func ExampleFormatUnified() {
+	diff := mdiff.New(
+		[]string{"I", "saw", "three", "mice", "running", "away"},
+		[]string{"three", "blind", "mice", "ran", "home"},
+	).AddContext(3).Unify()
+	mdiff.FormatUnified(os.Stdout, diff, &mdiff.FormatOptions{
+		OmitHeader: true,
+	})
+	// Output:
+	//
+	// @@ -1,6 +1,5 @@
+	// -I
+	// -saw
+	//  three
+	// +blind
+	//  mice
+	// -running
+	// -away
+	// +ran
+	// +home
+}
