e46a1d1d8eb6b4c726f1cd8d2744f8c9b1394f94
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-04-27 10:30:02 -0700

    slice: add basic benchmarks for LCS and EditScript

---
+++ slice/bench_test.go
@@ -0,0 +1,38 @@
+package slice_test
+
+import (
+	"flag"
+	"math/rand"
+	"testing"
+
+	"github.com/creachadair/mds/slice"
+)
+
+var (
+	lhsSize = flag.Int("lhs", 500, "LHS input size (number of elements)")
+	rhsSize = flag.Int("rhs", 500, "RHS input size (number of elements)")
+)
+
+func BenchmarkEdit(b *testing.B) {
+	lhs := make([]int, *lhsSize)
+	for i := range lhs {
+		lhs[i] = rand.Intn(1000000)
+	}
+	rhs := make([]int, *rhsSize)
+	for i := range rhs {
+		rhs[i] = rand.Intn(10000000)
+	}
+
+	b.Run("LCS", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_ = slice.LCS(lhs, rhs)
+		}
+	})
+	b.Run("EditScript", func(b *testing.B) {
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			_ = slice.EditScript(lhs, rhs)
+		}
+	})
+}
