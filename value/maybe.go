a98227161500a014770c65b3798603a9387254a2
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-06-04 12:46:28 -0700

    value: add a Maybe[T] type
    A Maybe[T] is a container for an optional single value of arbirary type, which
    may be either present or absent.

---
+++ value/maybe.go
@@ -0,0 +1,63 @@
+package value
+
+import "fmt"
+
+// Maybe is a container that can hold a value of type T, which may either be
+// present or absent. A zero value is ready for use, and is absent.
+//
+// It is safe to copy a Maybe value, but note that if a value is present, only
+// a shallow copy of the underlying value is made. Maybe values are comparable
+// if and only if T is comparable.
+type Maybe[T any] struct {
+	value   T
+	present bool
+}
+
+// Just returns a Maybe whose present value is v.
+func Just[T any](v T) Maybe[T] { return Maybe[T]{value: v, present: true} }
+
+// Absent returns an absent Maybe of the specified type.  It is a legibility
+// notation eauivalent to the zero value of type Maybe[T].
+func Absent[T any]() Maybe[T] { return Maybe[T]{} }
+
+// Present reports whether a value is present in m.
+func (m Maybe[T]) Present() bool { return m.present }
+
+// GetOK reports whether a value is present in m, and if so returns that value.
+// If a value is not present, GetOK returns the zero of T.
+func (m Maybe[T]) GetOK() (T, bool) { return m.value, m.present }
+
+// Get returns the value present in m, if any; or else the zero value.
+func (m Maybe[T]) Get() T { return m.value }
+
+// String returns the string representation of m.  If m is present, its string
+// representation is that of the enclosed value.
+func (m Maybe[T]) String() string {
+	if m.present {
+		return fmt.Sprint(m.value)
+	}
+	return fmt.Sprintf("Absent[%T]", m.value)
+}
+
+// MapMaybe returns a function from Maybe[T] to Maybe[U].
+// If the argument is present and has value v, the result is present and has
+// value f(v).  Otherwise, the result is absent and f is not called.
+func MapMaybe[T, U any](f func(T) U) func(Maybe[T]) Maybe[U] {
+	return func(a Maybe[T]) Maybe[U] {
+		if v, ok := a.GetOK(); ok {
+			return Just(f(v))
+		}
+		return Absent[U]()
+	}
+}
+
+// First returns the first present value in vs, if any exists; otherwise it
+// returns absent.
+func First[T any](vs ...Maybe[T]) Maybe[T] {
+	for _, v := range vs {
+		if v.Present() {
+			return v
+		}
+	}
+	return Maybe[T]{}
+}
---
+++ value/maybe_test.go
@@ -0,0 +1,90 @@
+package value_test
+
+import (
+	"testing"
+
+	"github.com/creachadair/mds/value"
+)
+
+func TestMaybe(t *testing.T) {
+	t.Run("Zero", func(t *testing.T) {
+		var v value.Maybe[int]
+		if v.Present() {
+			t.Error("Zero maybe should not be present")
+		}
+		if got := v.Get(); got != 0 {
+			t.Errorf("Get: got %d, want 0", got)
+		}
+	})
+
+	t.Run("Present", func(t *testing.T) {
+		v := value.Just("apple")
+		if got, ok := v.GetOK(); !ok || got != "apple" {
+			t.Errorf("GetOK: got %q, %v; want apple, true", got, ok)
+		}
+		if got := v.Get(); got != "apple" {
+			t.Errorf("Get: got %q, want apple", got)
+		}
+		if !v.Present() {
+			t.Error("Value should be present")
+		}
+	})
+
+	t.Run("String", func(t *testing.T) {
+		v := value.Just("pear")
+		if got := v.String(); got != "pear" {
+			t.Errorf("String: got %q, want pear", got)
+		}
+
+		var w value.Maybe[string]
+		if got, want := w.String(), "Absent[string]"; got != want {
+			t.Errorf("String: got %q, want %q", got, want)
+		}
+	})
+}
+
+func TestMapMaybe(t *testing.T) {
+	length := value.MapMaybe(func(s string) int {
+		return len(s)
+	})
+
+	tests := []struct {
+		input value.Maybe[string]
+		want  value.Maybe[int]
+	}{
+		{value.Just(""), value.Just(0)},
+		{value.Just("plum"), value.Just(4)},
+		{value.Absent[string](), value.Absent[int]()},
+	}
+	for _, tc := range tests {
+		if got := length(tc.input); got != tc.want {
+			t.Errorf("Length %q: got %v, want %v", tc.input, got, tc.want)
+		}
+	}
+}
+
+func TestFirst(t *testing.T) {
+	type tv = value.Maybe[int]
+	var absent tv
+	v1 := value.Just(1)
+	v2 := value.Just(2)
+	tests := []struct {
+		input []tv
+		want  tv
+	}{
+		{nil, absent},
+		{[]tv{}, absent},
+		{[]tv{absent}, absent},
+		{[]tv{absent, absent, absent}, absent},
+
+		{[]tv{v1}, v1},
+		{[]tv{absent, v1}, v1},
+		{[]tv{absent, v1, v2}, v1},
+		{[]tv{absent, absent, v1, absent, v2}, v1},
+	}
+	for _, tc := range tests {
+		if got := value.First(tc.input...); got != tc.want {
+			t.Errorf("First %+v: got %v, want %v", tc.input, got, tc.want)
+		}
+	}
+}
