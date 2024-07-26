725975ead55d5300e62eb50820daa8ead9f4163e
David Anderson <dave@natulte.net>
2024-07-25 17:11:41 -0700

    slice: remove example for deprecated Dedup

--- slice/example_test.go
+++ slice/example_test.go
@@ -18,13 +18,6 @@ func ExamplePartition() {
 	// [3 1 9 5 7]
 }
 
-func ExampleDedup() {
-	vs := []int{1, 1, 3, 2, 2, 4, 4, 5, 1, 3, 3, 3}
-	fmt.Println(slice.Dedup(vs))
-	// Output:
-	// [1 3 2 4 5 1 3]
-}
-
 func ExampleSplit() {
 	vs := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
