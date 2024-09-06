f722a62236073b140da220e2c524be776a0f7f07
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-09-05 18:34:10 -0700

    cache: a generic key/value cache implementation (#19)
    The cache package implements a concurrency-safe Cache type for comparable Key
    and arbitrary Value types. Sizing and eviction policy are configurable; the
    package currently provides a heap-based LRU implementation.

---
+++ cache/example_test.go
@@ -0,0 +1,42 @@
+package cache_test
+
+import (
+	"fmt"
+
+	"github.com/creachadair/mds/cache"
+)
+
+func Example() {
+	c := cache.New(10, cache.LRU[string, int](), nil)
+	for i := range 50 {
+		c.Put(fmt.Sprint(i+1), i+1)
+	}
+
+	fmt.Println("size:", c.Size())
+
+	fmt.Println("has 1:", c.Has("1"))
+	fmt.Println("has 40:", c.Has("40"))
+	fmt.Println("has 41:", c.Has("41"))
+	fmt.Println("has 50:", c.Has("50"))
+
+	fmt.Println(c.Get("41")) // access the value
+
+	c.Put("51", 51)
+
+	fmt.Println("has 42:", c.Has("42")) // gone now
+	fmt.Println(c.Get("41"))            // still around
+
+	c.Clear()
+	fmt.Println("size:", c.Size())
+
+	// Output:
+	// size: 10
+	// has 1: false
+	// has 40: false
+	// has 41: true
+	// has 50: true
+	// 41 true
+	// has 42: false
+	// 41 true
+	// size: 0
+}
