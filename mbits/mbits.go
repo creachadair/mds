b2d799ba30a5b16da82d4ec95bb37e0b0ce81032
M. J. Fromberger <michael.j.fromberger@gmail.com>
2025-07-15 08:07:35 -0700

    mbits: make better use of alignment

--- mbits/mbits.go
+++ mbits/mbits.go
@@ -35,17 +35,20 @@ func LeadingZeroes(data []byte) int {
 // TrailingZeroes reports the number of trailing zero bytes at the end of data.
 func TrailingZeroes(data []byte) int {
 	n := len(data)
+	m := n &^ 7 // end of full 64-bit chunks spanned by data
 
-	// Find the start of the tail of data comprising only full-width chunks.
-	//
-	//   | < 8 | ... 8 ... | ... 8 ... | . . . | ... 8 ... |
-	//         ^m                                          ^n
-	//
-	// Walk backward through these looking for a non-zero.
-	m := n - n&^7
+	// Note that we still take 64-bit chunks starting from offset 0, because the
+	// allocator is likely to have aligned the slice. We count the ragged tail
+	// first (if there is one) and then process strides, as above, but in
+	// reverse order.
 
-	i, nz := n-8, 0
-	for ; i >= m; i -= 8 {
+	var nz int
+	for n > m && data[n-1] == 0 {
+		nz++
+		n--
+	}
+
+	for i := n - 8; i >= 0; i -= 8 {
 		v := *(*uint64)(unsafe.Pointer(&data[i]))
 		if v != 0 {
 			// Count zeroes at the end of v.
@@ -56,11 +59,6 @@ func TrailingZeroes(data []byte) int {
 			return nz
 		}
 		nz += 8
-	}
-
-	// Count zeroes left at the tail of the ragged block at the front of data.
-	for m--; m >= 0 && data[m] == 0; m-- {
-		nz++
 	}
 	return nz
 }
